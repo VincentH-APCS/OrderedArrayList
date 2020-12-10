ef9bdb10db117e88c7ae6a01d523b79da321ecf6
VincentH-APCS <vhuang10@stuy.edu>
2020-12-10 15:33:45 -0500

    Tested for nulls

--- Demo.java
+++ Demo.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 public class Demo{
   public static void main(String[] args){
   NoNullArrayList<Integer> a = new NoNullArrayList<Integer>();
-  //Integer N = null;
+  Integer N = null;
 
   System.out.println(a.add(5));
   a.add(1,2);
   System.out.println(a.add(55));
-  //System.out.println(a.add(N));
+  System.out.println(a.set(1, N));
   }
 }
