2634a7c455a14ab5d2845f1a43352e38be65677b
VincentH-APCS <vhuang10@stuy.edu>
2020-12-10 17:16:14 -0500

    tested with adding nulls and with strings instead of integers

--- Demo.java
+++ Demo.java
@@ -1,12 +1,29 @@
 import java.util.ArrayList;
 public class Demo{
   public static void main(String[] args){
-  NoNullArrayList<Integer> a = new NoNullArrayList<Integer>();
+  OrderedArrayList<String> a = new OrderedArrayList<String>();
   Integer N = null;
 
-  System.out.println(a.add(5));
-  a.add(1,2);
-  System.out.println(a.add(55));
-  System.out.println(a.set(1, N));
+  a.add(0, "aaa");
+  System.out.println(a.add("bbb"));
+  System.out.println(a.add("yyy"));
+  System.out.println(a.add("rrd"));
+  System.out.println(a.add("mml"));
+  System.out.println(a.add("5pp"));
+  System.out.print(a.get(0));
+  System.out.print(a.get(1));
+  System.out.print(a.get(2));
+  System.out.print(a.get(3));
+  System.out.print(a.get(4));
+  System.out.print(a.get(5));
+  System.out.println();
+  System.out.println(a.set(0, "Konami"));
+  System.out.print(a.get(0));
+  System.out.print(a.get(1));
+  System.out.print(a.get(2));
+  System.out.print(a.get(3));
+  System.out.print(a.get(4));
+  System.out.print(a.get(5));
+  //System.out.println(a.set(4, N));
   }
 }
