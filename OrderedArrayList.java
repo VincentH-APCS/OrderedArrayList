2b915af2e8b12327149664ed6c886d2bbc7980d9
VincentH-APCS <vhuang10@stuy.edu>
2020-12-13 01:30:17 -0500

    Made a few minor fixes to the code after using the testers of Reng Zheng, most notably though, added to add(index,element) and set() so that it threw an IndexOutOfBoundsException if the inputted index was larger than the size of the OrderedArrayList

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -15,8 +15,11 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   }
 
   public void add(int index, T element){
+    if(index > this.size()){
+      throw new IndexOutOfBoundsException("Index larger than size of the Ordered Array List");
+    }
     super.add(index, element);
-    super.remove(this.size() - 1);
+    super.remove(index);
     int ind = 0;
     for(int i = 0; i < this.size(); i++){
       if(this.get(i).compareTo(element) >= 0){
@@ -28,6 +31,9 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   }
 
   public T set(int index, T element){
+    if(index > this.size()){
+      throw new IndexOutOfBoundsException("Index larger than size of the Ordered Array List");
+    }
     super.add(element);
     super.remove(this.size() - 1);
     int ind = 0;
@@ -37,12 +43,10 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
       }
       else ind++;
     }
-    super.add(ind, element);
-    if(ind < index){
-      return this.remove(index + 1);
-    }
-    else return this.remove(index);
-
+    T ans = super.get(index);
+    super.remove(index);
+    this.add(ind,element);
+    return ans;
   }
 
   public OrderedArrayList(){
